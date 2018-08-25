22948868ebc604fc2e6aa18898ce29765102b5be
fangzhipeng6 <42667705+fangzhipeng6@users.noreply.github.com>
2018-08-25 12:09:22 +0800

    Create login.js

---
+++ login.js
@@ -0,0 +1,24 @@
+function on_click2(){	
+	register=document.getElementById("register");	
+	register.style.backgroundColor="#000";
+	register.style.color="#0f0";
+	login.style.backgroundColor="#2acbaa";
+	login.style.color="#000";
+}
+
+function on_click1(){	
+	login=document.getElementById("login");
+	login.style.backgroundColor="#000";
+	login.style.color="#0f0";
+	register.style.backgroundColor="#2bc0b8";
+	register.style.color="#000";
+}
+
+function click_login(){	
+	var sub=document.getElementById("sub");
+	if(confirm("是否确认提交？")){
+		
+	}else{
+		
+	}
+}
